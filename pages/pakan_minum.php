a1ab1325f9f42ecdce74ab7582f6b3ad39388fa0
DragonGans <bimaapriliansyah445@gmail.com>
2026-05-06 07:24:15 +0700

    pake sidebar.php

--- pages/pakan_minum.php
+++ pages/pakan_minum.php
@@ -23,7 +23,6 @@ $topbarSubtitle = 'Pantau stok, aksi cepat, dan jadwal pemberian pakan minum';
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-<<<<<<< HEAD
   <link rel="stylesheet" href="../assets/dashboard.css">
   <style>
     .status-overview {
